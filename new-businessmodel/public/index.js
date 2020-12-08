921ae2af54e4c36e4316d0a35b495875d5fd26dd
Roelf-Jan de Vries <roelfjan@q42.nl>
2020-12-08 16:41:11 +0100

    Added transactions to Firebase

--- new-businessmodel/public/index.js
+++ new-businessmodel/public/index.js
@@ -1,10 +1,13 @@
 const THRESHOLD = 0.03;
 const PRICE = 0.05;
 const WALLET_ADDRESS = '$ilp.uphold.com/XYmDAJbMwE7Y';
+const MICRIO_ID = 'ERzWW';
 
 const META_TAG = document.createElement('meta');
 META_TAG.setAttribute('name', 'monetization');
 META_TAG.setAttribute('content', WALLET_ADDRESS);
+
+var db = firebase.firestore();
 
 var app = new Vue({
   el: '#app',
@@ -22,7 +25,7 @@ var app = new Vue({
     },
     addMicrio() {
       this.micrio = new Micrio({
-        id: 'ERzWW',
+        id: MICRIO_ID,
         container: document.getElementById('micrio'),
       });
     },
@@ -38,6 +41,24 @@ var app = new Vue({
       this.monetizationDenied = true;
       this.closeModal();
     },
+    sendTransactionToFirebase(event) {
+      // firebase gives errors on properties with value undefined
+      if (!event.detail.receipt) {
+        delete event.detail.receipt;
+      }
+
+      db.collection('transactions').add({
+        micrioId: MICRIO_ID,
+        event: event.detail,
+        timestamp: Date.now()
+      })
+      .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+    }
   },
   mounted() {
     if (PRICE > THRESHOLD) {
@@ -46,5 +67,7 @@ var app = new Vue({
       this.addMonetization();
       this.addMicrio();
     }
+
+    document.monetization.addEventListener('monetizationprogress', this.sendTransactionToFirebase);
   },
 });
